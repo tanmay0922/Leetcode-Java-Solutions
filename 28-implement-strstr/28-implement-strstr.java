a15fcf34d16358cff00a6e2ee3ea3fa7c5a269e9
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2022-08-31 01:23:09 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (80.56%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,8 @@
+//Basic String function
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0 )
+            return 0;
+        return haystack.indexOf(needle);
+    }
+}
