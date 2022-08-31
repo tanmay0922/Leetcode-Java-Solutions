a3c57a9547ed69ce77485f4728f8430d05c31b49
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2022-08-31 15:22:48 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (92.89%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int m=mat.length,n=mat[0].length;
+       /* int m=mat.length,n=mat[0].length;
         if(m*n != r*c)//Sizes check
             return mat;
         
@@ -22,5 +22,27 @@ class Solution {
         }
         
         return ans;
+    }
+}*/
+//2nd approach:
+         int[][] m = new int[r][c]; // make new matrix of r*c.
+        int row=0;
+        int col=0;
+        if(mat.length*mat[0].length!=r*c) return mat;
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                
+             m[row][col]=mat[i][j]; // here we are starting from 00 position and we want to increase horizontally in the same row that's why we are increasing column only not row. but at some point we will reach to the last column then we should go to next row and then again go to horizontally.
+                col++;
+                if(col==c){
+// here when we reach to the last column of first row then we will increase row by 1 and set our column to 0 so that we can repeat the same process.
+                     row++;
+                     col=0;
+                }
+              
+            }
+        }
+       return m; 
+        // by this process we can traverse our matrix by row by row 
     }
 }
