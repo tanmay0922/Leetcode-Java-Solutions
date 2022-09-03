9a0f48a5834bd847886316bced8e40de0151251d
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2022-09-03 11:01:12 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (23.40%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        /*String w="";int c=0;
+        for(int i=0;i<s.length();i++){
+          char ch=  s.charAt(i);
+            w=w+ch;
+            if(ch.isWhitespace())
+                c++;
+        }
+        return c;
+    }
+}*/
+        //Basic Approach
+       int n=s.length();
+        while(n>0 &&s.charAt(n-1)==' ')
+            n--;
+        int i=n-1;
+        while(i > -1 &&s.charAt(i)!=' ')
+            i--;
+        return n-1-i;
+    }
+}
