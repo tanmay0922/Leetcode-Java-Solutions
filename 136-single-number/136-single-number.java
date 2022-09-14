36356d716aadb6592a91a5975cb4260e597c9db8
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2022-09-14 19:38:27 +0530

    Time: 2 ms (68.15%), Space: 50.8 MB (39.62%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
+       /* Arrays.sort(nums);
         
         
 for(  int i = 0; i < nums.length-1 ;  i=i+2){
@@ -12,5 +12,14 @@ continue;//Just to break one iteration in the loop and start with another one.
 }
 }
 return nums[nums.length - 1];
+}
+}*/
+//Hence we can also use Bitwisexor gate technique
+ int count=0;
+    
+    for(int i=0;i<nums.length;i++)
+        count=count^nums[i];
+    
+    return count;
 }
 }
