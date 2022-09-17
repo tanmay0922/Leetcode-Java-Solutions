ea2677f584b3eec4e221013049b56add2a21a5a8
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2022-09-17 21:33:48 +0530

    Time: 109 ms (40.03%), Space: 100.3 MB (55.03%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int n = intervals.length;//Length of matrix
+      /*  int n = intervals.length;//Length of matrix
         
         if(n == 0) { //If n is i.e.length is 0 then the value returned is 0
             return 0;
@@ -23,5 +23,17 @@ class Solution {
         }
         
         return nonOverlappingIntervals;
+    }
+}*/
+         Arrays.sort(intervals,(v1,v2)->(v1[1]-v2[1])); //Sorting the arrays based on interval
+        int count = 1;
+        int end = intervals[0][1]; //ending index
+        for(int i=1;i<intervals.length;i++){ //loop
+            if(intervals[i][0]>=end){ //checking with ending index
+                count++;
+                end = intervals[i][1];
+            }
+        }
+        return intervals.length-count;
     }
 }
