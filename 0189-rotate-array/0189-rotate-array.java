9165cb79737a2ec755bb91189a17464442f33eb8
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2022-10-12 20:45:38 +0530

    Time: 3 ms (35.63%), Space: 64.3 MB (52.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] ans=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            ans[(i+k)%nums.length]=nums[i];
+        int[] ans=new int[nums.length]; //ans array
+        for(int i=0;i<nums.length;i++){ //loop till nums.length
+            ans[(i+k)%nums.length]=nums[i]; //calculating with the formula and storing in nums[i]
         }
-        for(int i=0; i<ans.length; i++){
-            nums[i]=ans[i];
+        for(int i=0; i<ans.length; i++){ //loop till answer.length
+            nums[i]=ans[i]; //making ans and nums as equal
         }
     }
 }
