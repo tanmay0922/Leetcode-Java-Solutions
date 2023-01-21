96f92edccb6fb4ef4b8a8da6799332da049752c5
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-01-21 21:43:53 +0530

    Time: 7 ms (45.25%), Space: 43.8 MB (14.93%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,6 +1,6 @@
 class Solution {
     public static List<String> restoreIpAddresses(String s) {
-        List<String> addresses = new ArrayList<>();
+      /*  List<String> addresses = new ArrayList<>();
         List<String> address = new ArrayList<>();
         rec(s, 0, address, addresses);
         return addresses;
@@ -18,6 +18,27 @@ class Solution {
                     rec(s, j, address, addresses);
                     address.remove(address.size() - 1);
                 }
+            }
+        }
+    }
+}*/
+        List<String>al=new ArrayList<>();
+        if(s.length()>12) return al;
+        help(0,s,al,new ArrayList<>());
+        return al;
+    }
+    public static void help(int ind,String s,List<String>al,ArrayList<String>ds){
+        if(ind==s.length()&&ds.size()==4){
+            al.add(String.join(".",ds));
+            return;
+        }
+        for(int i=1;i<=3;i++){
+            if(ind+i>s.length()) return;
+            String add=s.substring(ind,ind+i);
+            if(!(add.charAt(0)=='0'&&add.length()>1||Integer.parseInt(add)>255)){
+                ds.add(add);
+                help(ind+i,s,al,ds);
+                ds.remove(ds.size()-1);
             }
         }
     }
