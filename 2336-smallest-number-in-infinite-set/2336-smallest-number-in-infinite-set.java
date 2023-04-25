9997ed643c3529e7605b49ad2e11f1cebc4f51d4
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-04-25 22:38:05 +0530

    Time: 11 ms (88.39%), Space: 43.4 MB (41.36%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -0,0 +1,40 @@
+class SmallestInfiniteSet {
+    private SortedSet<Integer> addedIntegers;
+    private Integer currentInteger;
+    
+    public SmallestInfiniteSet() {
+        addedIntegers = new TreeSet<>();
+        currentInteger = 1;
+    }
+    
+    public int popSmallest() {
+        int answer;
+        // If there are numbers in the sorted-set, 
+        // top element is lowest among all the available numbers.
+        if (!addedIntegers.isEmpty()) {
+            answer = addedIntegers.first();
+            addedIntegers.remove(answer);
+        } 
+        // Otherwise, the smallest number of large positive set 
+        // denoted by 'currentInteger' is the answer.
+        else {
+            answer = currentInteger;
+            currentInteger += 1;
+        }
+        return answer;
+    }
+    
+    public void addBack(int num) {
+        if (currentInteger <= num || addedIntegers.contains(num)) {
+            return;
+        }
+        // We push 'num' in the sorted-set if it isn't already present.
+        addedIntegers.add(num);
+    }
+}
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
