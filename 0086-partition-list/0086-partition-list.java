49c31fc8a9d8f30580a960b681f82cb526c8ef98
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-08-16 01:13:07 +0530

    Time: 0 ms (100.00%), Space: 41 MB (60.57%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,7 +10,8 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head==null||head.next==null)
+        //Base case
+       /* if(head==null||head.next==null)
             return head;
         ListNode start=null,fresh,prev=null;
         for(ListNode ptr=head;ptr!=null;ptr=ptr.next)
@@ -39,4 +40,31 @@ class Solution {
         }
         return start;
     }
+}*/
+        ListNode small=new ListNode(0);
+        ListNode higher=new ListNode(0);
+        ListNode smallHead=small;
+        ListNode higherHead=higher;
+        while(head!=null){
+            if(head.val<x){
+                //small List
+                smallHead.next=head;
+                smallHead=smallHead.next;   
+        }
+        else
+           {
+                 //higher List
+                higherHead.next=head;
+                higherHead=higherHead.next;
+            }
+        head=head.next;
+    }
+    higherHead.next=null;
+    smallHead.next=higher.next;
+    return small.next;
 }
+}
+
+    
+        
+        
