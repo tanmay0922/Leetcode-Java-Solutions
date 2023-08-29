0f05019a85b494a4c0f244158d60d42b831d5946
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-08-29 23:56:07 +0530

    Time: 8 ms (98.65%), Space: 43.6 MB (99.66%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -6,17 +6,11 @@ class Solution {
 
         for (int i = 0; i < customers.length(); i++) {
             char ch = customers.charAt(i);
-            
-            // If status in hour i is 'Y', moving it to open hours decrement
-            // penalty by 1. Otherwise, moving 'N' to open hours increment
-            // penatly by 1.
             if (ch == 'Y') {
                 curPenalty--;
             } else {
                 curPenalty++;
             }
-
-            // Update earliestHour if a smaller penatly is encountered.
             if (curPenalty < minPenalty) {
                 earliestHour = i + 1;
                 minPenalty = curPenalty;
