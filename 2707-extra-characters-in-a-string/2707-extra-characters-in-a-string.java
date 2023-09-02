4f0f0b40e68db985f2fa8ea09a94d3ff12cd512a
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-09-02 23:18:29 +0530

    Time: 42 ms (73.43%), Space: 44.1 MB (86.29%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -0,0 +1,48 @@
+/*class Solution {
+    Integer[] memo;
+    HashSet<String> dictionarySet;
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        memo = new Integer[n];
+        dictionarySet = new HashSet<>(Arrays.asList(dictionary));
+        return dp(0, n, s);
+    }
+    private int dp(int start, int n, String s) {
+        if (start == n) {
+            return 0;
+        }
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        // To count this character as a left over character 
+        // move to index 'start + 1'
+        int ans = dp(start + 1, n, s) + 1;
+        for (int end = start; end < n; end++) {
+            var curr = s.substring(start, end + 1);
+            if (dictionarySet.contains(curr)) {
+                ans = Math.min(ans, dp(end + 1, n, s));
+            }
+        }
+
+        return memo[start] = ans;
+    }
+}*/
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        var dictionarySet = new HashSet<>(Arrays.asList(dictionary));
+        var dp = new int[n + 1];
+
+        for (int start = n - 1; start >= 0; start--) {
+            dp[start] = dp[start + 1] + 1;
+            for (int end = start; end < n; end++) {
+                var curr = s.substring(start, end + 1);
+                if (dictionarySet.contains(curr)) {
+                    dp[start] = Math.min(dp[start], dp[end + 1]);
+                }
+            }
+        }
+
+        return dp[0];
+    }
+}
