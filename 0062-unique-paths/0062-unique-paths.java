0529514672b7f9d287ded0e73b50f25f8b096e8d
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-09-03 19:05:38 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (66.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,10 +1,28 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        long ans=1;
+      /*  long ans=1;
         for(int i=1;i<=m-1;i++){
             ans=ans*(n-1+i)/i;
             
         }
         return (int) ans;
+    }
+}*/
+int[][] dp = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 }
