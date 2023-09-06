d89d1102e6dda47c4b0c648be3f1f3a5bd322ba2
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-09-06 19:22:37 +0530

    Time: 4 ms (96.18%), Space: 54.5 MB (65.58%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int n=prices.length;
+      /*  int n=prices.length;
         int [] feex=new int[n],hold =new int[n];
         hold[0]=-prices[0];
         for(int i=1;i<n;i++){
@@ -8,5 +8,15 @@ class Solution {
             feex[i]=Math.max(feex[i-1],hold[i-1]+prices[i]-fee);
         }
         return feex[n-1];
+    }
+}*/
+        int n=prices.length;
+        int feex=0,hold=-prices[0];
+        for(int i=1;i<n;i++){
+            int tmp=hold;
+            hold=Math.max(hold,feex-prices[i]);
+            feex=Math.max(feex,tmp+prices[i]-fee);
+        }
+        return feex;
     }
 }
