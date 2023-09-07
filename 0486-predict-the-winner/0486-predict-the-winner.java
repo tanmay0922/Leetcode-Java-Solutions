b37d86724b826529c4fd8f4638385af2b913993d
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-09-07 20:59:31 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (99.60%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     private int maxDiff(int[] nums, int left, int right, int n) {
         if (left == right) {
             return nums[left];
@@ -15,4 +15,19 @@ class Solution {
         
         return maxDiff(nums, 0, n - 1, n) >= 0;
     }
-}
+}*/
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = Arrays.copyOf(nums, n);
+        
+        for (int diff = 1; diff < n; ++diff) {
+            for (int left = 0; left < n - diff; ++left) {
+                int right = left + diff;
+                dp[left] = Math.max(nums[left] - dp[left + 1], nums[right] - dp[left]);
+            }
+        }
+        
+        return dp[0] >= 0;
+    }
+}   
