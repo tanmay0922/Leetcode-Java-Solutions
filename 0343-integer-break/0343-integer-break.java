f3677a791d4f6ac0b9fa58a0f43ed415299da0a4
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-10-06 23:24:23 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (17.26%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,45 @@
+/*class Solution {
+    int[] memo;
+    
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        
+        memo = new int[n + 1];
+        return dp(n);
+    }
+    
+    public int dp(int num) {
+        if (num <= 3) {
+            return num;
+        }
+        
+        if (memo[num] != 0) {
+            return memo[num];
+        }
+        
+        int ans = num;
+        for (int i = 2; i < num; i++) {
+            ans = Math.max(ans, i * dp(num - i));
+        }
+        
+        memo[num] = ans;
+        return ans;
+    }
+}*/
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        
+        int ans = 1;
+        while (n > 4) {
+            ans *= 3;
+            n -= 3;
+        }
+        
+        return ans * n;
+    }
+}
