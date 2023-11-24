a3575b73348c359626e6e6a99ef5782edd22857c
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-11-24 21:08:32 +0530

    Time: 27 ms (98.94%), Space: 54.7 MB (61.73%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxCoins(int[] piles) {
-        Arrays.sort(piles);
+       /* Arrays.sort(piles);
         ArrayDeque<Integer> queue = new ArrayDeque();
         for (int num : piles) {
             queue.addLast(num);
@@ -13,6 +13,14 @@ class Solution {
             queue.removeFirst(); // bob
         }
         
+        return ans;
+    }
+}*/
+        Arrays.sort(piles);
+        int ans=0;
+        for(int i=piles.length/3;i<piles.length;i+=2){
+            ans+=piles[i];
+        }
         return ans;
     }
 }
