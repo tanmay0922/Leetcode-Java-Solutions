21c32e9d380155a8b08eee881ec58418d1442bbc
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2023-12-20 23:37:34 +0530

    Time: 2 ms (77.87%), Space: 44.2 MB (5.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,32 +1,11 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        // Assume Minimum Cost to be Infinity
-        int minCost = Integer.MAX_VALUE;
+        Arrays.sort(prices);
+        int minCost = prices[0] + prices[1];
 
-        // Number of Chocolates
-        int n = prices.length;
-
-        // Check Every Pair of Chocolates
-        for (int firstChoco = 0; firstChoco < n; firstChoco++) {
-            for (int secondChoco = firstChoco + 1; secondChoco < n; secondChoco++) {
-                // Sum of Prices of the Two Chocolates
-                int cost = prices[firstChoco] + prices[secondChoco];
-
-                // If the Sum of Prices is Less than the Minimum Cost
-                if (cost < minCost) {
-                    // Update the Minimum Cost
-                    minCost = cost;
-                }
-            }
-        }
-
-        // We can buy chocolates only if we have enough money
         if (minCost <= money) {
-            // Return the Amount of Money Left
             return money - minCost;
-        } else {
-            // We cannot buy chocolates. Return the initial amount of money
-            return money;
         }
+        return money;
     }
 }
