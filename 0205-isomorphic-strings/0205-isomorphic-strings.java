5ae54de4d657107077262c267d005985409e4308
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2024-04-02 23:30:29 +0530

    Time: 7 ms (89.77%), Space: 42.2 MB (67.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-    int map1[]=new int[200];
-        int map2[]=new int[200];
-        if(s.length()!=t.length())
+        // Create arrays to store the index of characters in both strings
+        int[] indexS = new int[200]; // Stores index of characters in string s
+        int[] indexT = new int[200]; // Stores index of characters in string t
+        
+        // Get the length of both strings
+        int len = s.length();
+        
+        // If the lengths of the two strings are different, they can't be isomorphic
+        if(len != t.length()) {
             return false;
-         for(int i=0;i<s.length();i++)
-        {
-            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
-                return false;
-
-            map1[s.charAt(i)]=i+1;
-            map2[t.charAt(i)]=i+1;
         }
+        
+        // Iterate through each character of the strings
+        for(int i = 0; i < len; i++) {
+            // Check if the index of the current character in string s
+            // is different from the index of the corresponding character in string t
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
+                return false; // If different, strings are not isomorphic
+            }
+            
+            // Update the indices of characters in both strings
+            indexS[s.charAt(i)] = i + 1; // updating index of current character
+            indexT[t.charAt(i)] = i + 1; // updating index of current character
+        }
+        
+        // If the loop completes without returning false, strings are isomorphic
         return true;
     }
 }
