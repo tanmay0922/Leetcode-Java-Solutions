deacd18da6b5e45f87dcaed0e0f39f5ede79bd4f
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2024-04-06 01:44:28 +0530

    Time: 3 ms (61.14%), Space: 42.2 MB (48.37%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,12 +1,20 @@
 class Solution {
     public String makeGood(String s) {
-    for (int i = 0; i < s.length() - 1; i++) {
-        //Now we check if the either the present or the next character is capital
-        if (Math.abs(s.charAt(i) - s.charAt(i+1)) == 32) {  
-            //recursive call for the rest of the string 
-            return makeGood(s.substring(0, i) + s.substring(i+2));
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
         }
-    }     
-    return s;
-}
+        
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+        
+        return result.toString();
     }
+}
