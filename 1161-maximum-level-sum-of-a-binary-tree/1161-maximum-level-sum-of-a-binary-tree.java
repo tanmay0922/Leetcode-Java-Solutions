a0375c7737d52cd1505fc84cc6855162737b3790
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2026-01-07 01:11:25 +0530

    Time: 9 ms (64.19%), Space: 49.4 MB (17.4%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -15,34 +15,31 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        int maxSum = Integer.MIN_VALUE;
-        int ans = 0, level = 0;
+     int maxSum =Integer.MIN_VALUE;
+     int ans = 0 , level =0;
+     Queue <TreeNode> q = new LinkedList<>();
+     q.offer(root); //offer does insertion of a specified element
+     while (!q.isEmpty()){
+        level ++;
+        int sumAtCurrentLevel =0;
+        for (int s= q.size();s>0;--s){
+            TreeNode node = q.poll();
+            sumAtCurrentLevel += node.val;
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            level++;
-            int sumAtCurrentLevel = 0;
-            // Iterate over all the nodes in the current level.
-            for (int sz = q.size(); sz > 0; --sz) {
-                TreeNode node = q.poll();
-                sumAtCurrentLevel += node.val;
-
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
+            if(node.left != null){
+                q.offer(node.left);
             }
-
-            if (maxSum < sumAtCurrentLevel) {
-                maxSum = sumAtCurrentLevel;
-                ans = level;
+            if(node.right != null){
+                q.offer(node.right);
             }
         }
-
+        if(maxSum < sumAtCurrentLevel){
+            maxSum =sumAtCurrentLevel;
+            ans=level;
+        }
+        
+            
+        }
         return ans;
+     }   
     }
-}
