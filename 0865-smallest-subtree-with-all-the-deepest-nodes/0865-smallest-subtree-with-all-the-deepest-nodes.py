23b0dece56a90e74829e435c44931ec66c8df9cb
Tanmay Upadhyay <89064782+tanmay0922@users.noreply.github.com>
2026-01-10 00:51:40 +0530

    Time: 0 ms (100%), Space: 19.5 MB (11.5%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subtreeWithAllDeepest(self, r: Optional[TreeNode]) -> Optional[TreeNode]:
+        def f(n):
+            if not n: return 0,None
+            ld,ln,rd,rn = *f(n.left),*f(n.right)
+            return ((ld+1,n),(ld+1,ln),(rd+1,rn))[(ld>rd)-(ld<rd)]
+
+        return f(r)[1]
